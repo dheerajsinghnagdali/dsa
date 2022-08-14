b7cb120a02723736b70aa0badc3656b63e4400cb
Dheeraj Singh Nagdali <dheerajsinghnagdali@gmail.com>
2022-08-14 17:05:27 +0530

    sol of josephus, power set, palindrome string, matrix paths

--- src/recursion.ts
+++ src/recursion.ts
@@ -32,4 +32,46 @@ function matrixPaths(row: number, col: number): number {
   return matrixPaths(row - 1, col) + matrixPaths(row, col - 1);
 }
 
+// Q. Josephus problem, matrix paths
+
+function josephus(n: number, k: number): number {
+  if (n === 1) {
+    return 0;
+  }
+  return (josephus(n - 1, k) + k) % n;
+}
+
+// Q. check given string is palindrome string
+
+function isPalindrome(str: string, left = 0, right = str.length - 1): boolean {
+  if (left >= right) {
+    return true;
+  }
+  if (str[left] !== str[right]) {
+    return false;
+  }
+  return isPalindrome(str, left + 1, right - 1);
+}
+
+// Q. print the powerset of a given string
+
+/**
+ *                     abc
+ *                   /    \
+ *                  a      ""
+ *                /  \
+ *               ab   a
+ *             /  \
+ *            abc  ab
+ */
+
+function powerSet(str: string, i = 0, curr = ""): void {
+  if (i === str.length) {
+    console.log(curr);
+    return;
+  }
+  powerSet(str, i + 1, curr + str[i]);
+  powerSet(str, i + 1, curr);
+}
+
 export {};
