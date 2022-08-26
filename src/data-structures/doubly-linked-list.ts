b3e21eb363ac00d995a01e0b232a8cdd348e192c
Dheeraj Singh Nagdali <dheerajsinghnagdali@gmail.com>
2022-08-27 00:29:53 +0530

    feat: add doubly linked list and hash table

---
+++ src/data-structures/doubly-linked-list.ts
@@ -0,0 +1,117 @@
+export class Node<T> {
+  next: Node<T> | null;
+  prev: Node<T> | null;
+  constructor(public value: T) {
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+export class DoublyLinkedList<T> {
+  head: Node<T> | null;
+  tail: Node<T> | null;
+  size: number;
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  isNotEmpty(): this is { tail: Node<T>; head: Node<T>; size: number } {
+    return this.size !== 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  prepend(value: T) {
+    const node = new Node(value);
+    if (this.isNotEmpty()) {
+      node.next = this.head;
+      this.head.prev = node;
+      this.head = node;
+    } else {
+      this.head = node;
+      this.tail = node;
+    }
+    this.size++;
+  }
+
+  append(value: T) {
+    const node = new Node(value);
+    if (this.isNotEmpty()) {
+      node.prev = this.tail;
+      this.tail.next = node;
+      this.tail = node;
+    } else {
+      this.head = node;
+      this.tail = node;
+    }
+  }
+
+  print() {
+    let result = "";
+    let node = this.head;
+    while (node) {
+      result += `${node.value}<->`;
+      node = node.next;
+    }
+    console.log(result);
+  }
+
+  removeFromFront() {
+    if (this.isNotEmpty()) {
+      const value = this.head.value;
+      this.head = this.head.next as Node<T>;
+      this.head.prev = null;
+      this.size--;
+      return value;
+    } else {
+      return null;
+    }
+  }
+
+  removeFromEnd() {
+    if (this.isNotEmpty()) {
+      const value = this.tail.value;
+      this.tail = this.tail.prev as Node<T>;
+      this.tail.next = null;
+      this.size--;
+      return value;
+    } else {
+      return null;
+    }
+  }
+
+  printReverse() {
+    if (this.isNotEmpty()) {
+      let curr: Node<T> | null = this.tail;
+      let result = "";
+      while (curr) {
+        result += `${curr.value}<->`;
+        curr = curr.prev;
+      }
+      console.log(result);
+    } else {
+      console.log("List is empty");
+    }
+  }
+}
+
+const list = new DoublyLinkedList<number>();
+list.print();
+list.prepend(10);
+list.prepend(20);
+list.prepend(30);
+list.prepend(40);
+
+list.print();
+
+list.removeFromFront();
+list.print();
+
+list.removeFromEnd();
+list.print();
+
+list.printReverse();
---
+++ src/data-structures/hash-table.ts
@@ -0,0 +1,70 @@
+export class HashTable<T> {
+  table: [string, T][][];
+  constructor(public size: number) {
+    this.table = new Array(size);
+  }
+
+  hash(key: string) {
+    let total = 0;
+    for (let i = 0; i < key.length; i++) {
+      total = key.charCodeAt(i);
+    }
+    return total % this.size;
+  }
+
+  set(key: string, value: T) {
+    const index = this.hash(key);
+    const bucket = this.table[index];
+
+    if (bucket) {
+      const sameKeyItem = bucket.find((item) => item[0] === key);
+      if (sameKeyItem) {
+        sameKeyItem[1] = value;
+      } else {
+        bucket.push([key, value]);
+      }
+    } else {
+      this.table[index] = [[key, value]];
+    }
+  }
+
+  get(key: string) {
+    const index = this.hash(key);
+    const bucket = this.table[index];
+    if (bucket) {
+      const sameKeyItem = bucket.find((item) => item[0] === key);
+      if (sameKeyItem) {
+        return sameKeyItem[1];
+      }
+    }
+  }
+
+  remove(key: string) {
+    const index = this.hash(key);
+    const bucket = this.table[index];
+    if (bucket) {
+      const filterdBucket = bucket.filter((item) => item[0] !== key);
+      this.table[index] = filterdBucket;
+    }
+  }
+
+  display() {
+    for (let i = 0; i < this.table.length; i++) {
+      const element = this.table[i];
+      if (element) {
+        console.log(i, element);
+      }
+    }
+  }
+}
+
+const ht = new HashTable<string | number>(10);
+ht.set("0", "neeraj");
+ht.set("1", "dheeraj");
+ht.set("0", "akash");
+
+console.log(ht.get("0"));
+console.log(ht.get("1"));
+ht.remove("0");
+
+ht.display();
